85e53e65b89acd59a65373df55b5633b62d29d99
Sagor <mbrsagor@gmail.com>
2021-01-02 23:00:58 +0600

    Task list add Django default admin template

--- task/admin.py
+++ task/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from task.models.category import Category
+from task.models.task import Task, TaskManager
 
 admin.site.register(Category)
+admin.site.register(Task)
+admin.site.register(TaskManager)
