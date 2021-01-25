ce46529798c109025b710c18efcd1873e4aa3b62
Sagor <mbrsagor@gmail.com>
2021-01-25 21:26:21 +0600

    Cateogry seralizer parent get all field

--- task/serializers/category_serializer.py
+++ task/serializers/category_serializer.py
@@ -6,4 +6,9 @@ from task.models.category import Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ('id', 'name', 'parent', 'order', 'banner', 'created_at', 'updated_at')
+        fields = ('id', 'name', 'parent', 'order', 'isActive', 'created_at', 'updated_at')
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['parent'] = CategorySerializer(instance.parent).data
+        return response
