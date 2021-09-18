3cbb5e60831b2eb4a7d4deebb76886c1f0769f10
Md.Bozlur Rosid Sagor <39002174+mbrsagor@users.noreply.github.com>
2021-09-18 21:30:41 +0600

    New: Views logic added

--- account/views.py
+++ account/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import views, status, permissions
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import User
+from .serializer import UserSerializer
+
+
+class UserAPIView(views.APIView):
+  permission_classes = [permissions.AllowAny, ]
+  
+  def post(self, request):
+    try:
+      pass
+    except Exception as e:
+        print(e)
+        return Response(
+            {"error": f"Something went to wrong registering and account. Exception: {e}"},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+  
