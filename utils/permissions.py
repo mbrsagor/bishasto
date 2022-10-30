3cfeefc9aeb2185e7fc3e073d89b3cc07a40b258
mbrsagor <mbrsagor@gmail.com>
2022-10-30 15:43:54 +0600

    New: Tenean dashboard add filter.

--- utils/permissions.py
+++ utils/permissions.py
@@ -2,13 +2,14 @@ from rest_framework.permissions import BasePermission, IsAuthenticated, SAFE_MET
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+
 class ReadOnly(BasePermission):
     def has_permission(self, request, view):
         return request.method in SAFE_METHODS
 
+
 class ExampleView(APIView):
-    permission_classes = [IsAuthenticated|ReadOnly]
+    permission_classes = [IsAuthenticated | ReadOnly]
 
     def get(self, request, format=None):
         return Response('success!')
-
