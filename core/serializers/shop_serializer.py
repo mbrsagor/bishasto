35c5c29761b49972458ede0737a842308c3fde95
mbrsagor <mbrsagor@gmail.com>
2023-05-13 00:09:51 +0600

    New: Average rating calulation done.

--- core/serializers/shop_serializer.py
+++ core/serializers/shop_serializer.py
@@ -11,3 +11,15 @@ class ShopSerializer(serializers.ModelSerializer):
             'is_available', 'tread_license', 'address', 'shop_created_date', 'owner_humanity',
             'phone_number', 'documents', 'shop_image', 'created_at', 'updated_at'
         ]
+
+    """
+        def get_averageRating(self, obj):
+        get_ratings = []
+        product_rating = ProductRating.objects.filter(product=obj)
+        ratings = ProductRatingSerializer(product_rating, many=True).data
+        # put only rating number
+        for rating in ratings:
+            get_ratings.append(rating['rating'])
+        result = sum(get_ratings) / len(get_ratings)
+        return int(result)
+    """
