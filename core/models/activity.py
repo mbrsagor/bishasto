d41d9f924b0a34c37b044aedc3d4c73d7f3d5370
mbrsagor <mbrsagor@gmail.com>
2023-11-08 22:58:19 +0600

    New: user activity db created.

---
+++ core/models/activity.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+
+class Action(models.Model):
+    sender = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
+    verb = models.CharField(max_length=255)
+    target_ct = models.ForeignKey(ContentType, blank=True, null=True,
+                                  related_name='target_obj', on_delete=models.CASCADE)
+    target_id = models.PositiveIntegerField(null=True, blank=True)
+    target = GenericForeignKey('target_ct', 'target_id')
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.pk
