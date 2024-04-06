59021dd472dd9f78e9c21ff52099baea47b0a108
mbrsagor <mbrsagor@gmail.com>
2024-04-06 20:00:39 +0600

    Refactor: Hot refactor.

--- utils/permissions.py
+++ utils/permissions.py
@@ -1,15 +1,65 @@
-from rest_framework.permissions import BasePermission, IsAuthenticated, SAFE_METHODS
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+from django.utils.translation import gettext_lazy as _
+from rest_framework.permissions import BasePermission
+
+from core.models.order import Order
 
 
-class ReadOnly(BasePermission):
+class IsOrderPending(BasePermission):
+    """
+    Check the status of order is pending or completed before updating/deleting instance
+    """
+    message = _('Updating or deleting closed order is not allowed.')
+
+    def has_object_permission(self, request, view, obj):
+        if view.action in ('retrieve',):
+            return True
+        return obj.status == 'P'
+
+
+class IsOrderItemByBuyerOrAdmin(BasePermission):
+    """
+    Check if order item is owned by appropriate buyer or admin
+    """
+
     def has_permission(self, request, view):
-        return request.method in SAFE_METHODS
+        order_id = view.kwargs.get('order_id')
+        order = get_object_or_404(Order, id=order_id)
+        return order.buyer == request.user or request.user.is_staff
+
+    def has_object_permission(self, request, view, obj):
+        return obj.order.buyer == request.user or request.user.is_staff
 
 
-class ExampleView(APIView):
-    permission_classes = [IsAuthenticated | ReadOnly]
+class IsOrderByBuyerOrAdmin(BasePermission):
+    """
+    Check if order is owned by appropriate buyer or admin
+    """
 
-    def get(self, request, format=None):
-        return Response('success!')
+    def has_permission(self, request, view):
+        return request.user.is_authenticated is True
+
+    def has_object_permission(self, request, view, obj):
+        return obj.buyer == request.user or request.user.is_staff
+
+
+class IsOrderItemPending(BasePermission):
+    """
+    Check the status of order is pending or completed before creating, updating and deleting order items
+    """
+    message = _(
+        'Creating, updating or deleting order items for a closed order is not allowed.')
+
+    def has_permission(self, request, view):
+        order_id = view.kwargs.get('order_id')
+        order = get_object_or_404(Order, id=order_id)
+
+        if view.action in ('list', ):
+            return True
+
+        return order.status == 'P'
+
+    def has_object_permission(self, request, view, obj):
+        if view.action in ('retrieve',):
+            return True
+        return obj.order.status == 'P'
