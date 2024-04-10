52e317c4cc95b00fcfb501153ffda54b51ad6669
mbrsagor <mbrsagor@gmail.com>
2024-04-10 23:35:16 +0600

    Remove: removed unsued code.

--- bishasto/settings.py
+++ bishasto/settings.py
@@ -112,13 +112,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join('db.sqlite3')
-#     }
-# }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
