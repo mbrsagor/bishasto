c9f08ea60cab3defe94bda53385ae22615ed8160
mbrsagor <mbrsagor@gmail.com>
2024-06-12 09:19:44 +0600

    New: Basic strip payment added.

---
+++ core/views/payment_view.py
@@ -0,0 +1,22 @@
+from rest_framework import views, status
+from rest_framework.response import Response
+from core.serializers.payment_serializer import CardInformationSerializer
+import stripe
+
+
+class PaymentAPI(views.APIView):
+    serializer_class = CardInformationSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        response = {}
+        if serializer.is_valid():
+            data_dict = serializer.data
+
+            stripe.api_key = 'your-key-goes-here'
+            response = self.stripe_card_payment(data_dict=data_dict)
+        else:
+            response = {'errors': serializer.errors, 'status':
+                status.HTTP_400_BAD_REQUEST
+                        }
+        return Response(response)
