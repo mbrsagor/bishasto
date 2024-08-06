43a047549c010ad4bd1bc77be4d54b69c82e4087
mbrsagor <mbrsagor@gmail.com>
2024-08-06 12:42:31 +0600

    New: simple example of verify ticket.

--- core/views/multiple_action.py
+++ core/views/multiple_action.py
@@ -32,3 +32,44 @@ def fuel_create_view(request):
         'fuel_from': fuel_from
     }
     return render(request, template_name, context)
+
+
+class VerifyTicketAPIView(views.APIView):
+    """
+    Name: Guard can verify ticket
+    Desc: Guard can verify ticket and send email to company
+    URL: /api/v1/event/verify-ticket/
+    Methods: PUT
+    :param: event_id, ticket_number
+    :return
+    """
+
+    def get(self, request, *args, **kwargs):
+        # Get data from request
+        event_id = self.request.query_params.get('event_id')
+        ticket_number = self.request.query_params.get('ticket_number')
+
+        try:
+            event = Event.objects.get(id=event_id)
+            ticket = event.eventTicket.get(ticket_number=ticket_number)
+            # Verify ticket
+            verify_ticket = Ticket.objects.get(ticket_number=ticket)
+            if verify_ticket.is_active == False and verify_ticket.is_scanned == False:
+                verify_ticket.is_active = True
+                verify_ticket.is_scanned = True
+                verify_ticket.save()
+                resp = {
+                    "status": "success",
+                    "message": messages.VERIFY_SUCCESS_TICKET,
+                    "is_verified": True,
+                }
+                return Response(resp, status=status.HTTP_200_OK)
+            resp2 = {
+                    "status": "success",
+                    "message": messages.VERIFY_FAILED_TICKET,
+                    "is_verified": False,
+                }
+            return Response(resp2, status=status.HTTP_200_OK)
+        except Exception as ex:
+            print(str(ex))
+            return Response(responses.prepare_error_response(messages.TICKET_NOT_MATCH), status=status.HTTP_200_OK)
