d22905cd9e33f65175b473095ab7480d37ac6df0
Md.Bozlur Rosid Sagor <39002174+mbrsagor@users.noreply.github.com>
2024-09-03 20:20:34 +0600

    New: Follower DB added

--- user/models.py
+++ user/models.py
@@ -88,3 +88,15 @@ class ResetPhoneOTP(AbstractUser):
 
     def __str__(self):
         return str(self.phone) + ' is sent ' + str(self.otp)
+
+
+class Follow(Timestamp):
+    follower = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+    following = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('follower', 'following')
+
+    def __str__(self):
+        return f"{self.follower} follows {self.following}"
